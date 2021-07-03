a5622f58a57e4d30a7741a30f10157d0f1fdd749
kkata <14849535+kkata@users.noreply.github.com>
2021-07-03 18:26:31 +0900

    Practice: conditional content

--- src/components/NexExpense/NewExpense.js
+++ src/components/NexExpense/NewExpense.js
@@ -1,20 +1,31 @@
-import React from "react";
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 const NewExpense = (props) => {
+  const [isFormVisible, setIsFormVisible] = useState(false);
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+    setIsFormVisible(!isFormVisible);
   };
-  return (
-    <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+  const toggleNewExpenseForm = () => {
+    setIsFormVisible(!isFormVisible);
+  };
+
+  const newExpenseContent = isFormVisible ? (
+    <ExpenseForm
+      onSaveExpenseData={saveExpenseDataHandler}
+      onCancel={toggleNewExpenseForm}
+    />
+  ) : (
+    <button onClick={toggleNewExpenseForm}>Add New Expense</button>
   );
+
+  return <div className="new-expense">{newExpenseContent}</div>;
 };
 
 export default NewExpense;

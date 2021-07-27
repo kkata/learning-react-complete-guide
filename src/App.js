d84683ead8665b9725cfc2055bf645af5b685c88
kkata <14849535+kkata@users.noreply.github.com>
2021-07-27 23:58:09 +0900

    starting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div></div>;
-};
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+    </div>
+  );
+}
 
 export default App;

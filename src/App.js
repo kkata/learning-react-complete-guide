334387d28eb004f1e6424e8906e793445027a592
kkata <14849535+kkata@users.noreply.github.com>
2021-07-30 20:54:36 +0900

    156. useCallback() and its Dependencies

--- src/App.js
+++ src/App.js
@@ -6,20 +6,32 @@ import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING");
 
   // useCallback() でButtonコンポーネント関数が実行されないように
+  // useCallback()は関数を保持して再生成させないようにする
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  }, []);
+    // dependenceの配列にallowToggleを入れないと変化を受け取れない
+    console.log(allowToggle);
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
   // const toggleParagraphHandler = () => {
   //   setShowParagraph((prevShowParagraph) => !prevShowParagraph);
   // };
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );

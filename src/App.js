91837e09e0a63c7679f96453e9d099ad4b5fd7a9
kkata <14849535+kkata@users.noreply.github.com>
2021-08-02 00:02:00 +0900

    175. Sending a GET Request

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
+
+import MovieList from "./components/MoviesList";
+
+import "./App.css";
+
 const App = () => {
-  return <div></div>;
+  const [movies, setMovies] = useState([]);
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+  return (
+    <>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MovieList movies={movies} />
+      </section>
+    </>
+  );
 };
 
 export default App;

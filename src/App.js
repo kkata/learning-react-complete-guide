e51bc54e91ce0033ed02b043d7ed606a7296682d
kkata <14849535+kkata@users.noreply.github.com>
2021-08-07 18:57:25 +0900

    192. Adjusting the Custom Hook Logic
    too confusing..

--- src/App.js
+++ src/App.js
@@ -7,26 +7,20 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const {
-    isLoading,
-    error,
-    sendRequest: fetchTasks,
-  } = useHttp({ url: process.env.REACT_APP_API }, transformTasks);
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []); // useEffect()の依存stateにfetchTaskを入れたくない（fetchが繰り返されるため）https://overreacted.io/ja/a-complete-guide-to-useeffect/#%E3%81%A7%E3%82%82%E3%80%81%E3%81%93%E3%81%AE%E9%96%A2%E6%95%B0%E3%81%AF%E3%82%A8%E3%83%95%E3%82%A7%E3%82%AF%E3%83%88%E5%86%85%E3%81%AB%E5%85%A5%E3%82%8C%E3%82%89%E3%82%8C%E3%81%AA%E3%81%84
-  // ベストな解決策ではないがここでは一旦空配列のままにする
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+    fetchTasks({ url: process.env.REACT_APP_API }, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));

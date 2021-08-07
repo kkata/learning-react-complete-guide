4f97b8599e2d6e150def4bb10bf6d3be141fa888
kkata <14849535+kkata@users.noreply.github.com>
2021-08-07 19:55:51 +0900

    193. Using The Custom Hook In More Components

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,38 +1,29 @@
-import { useState } from "react";
-
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(process.env.REACT_APP_API, {
+    sendTaskRequest(
+      {
+        url: process.env.REACT_APP_API,
         method: "POST",
-        body: JSON.stringify({ text: taskText }),
+        body: { text: taskText },
         headers: {
           "Content-Type": "application/json",
         },
-      });
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+      },
+      createTask.bind(null, taskText) // .bind()は1番目の引数にthisに何を割り当てるか→thisは使わないのでnull,2番目に渡したい値を設定する→元の関数の1番目の引数にアサインされる
+    );
   };
 
   return (

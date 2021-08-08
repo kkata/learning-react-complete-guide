e9be61dc4b8936f482c4a9eb9030baee67f2b2a7
kkata <14849535+kkata@users.noreply.github.com>
2021-08-08 18:17:16 +0900

    added starting files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div></div>;
-};
+import SimpleInput from './components/SimpleInput';
+
+function App() {
+  return (
+    <div className="app">
+      <SimpleInput />
+    </div>
+  );
+}
 
 export default App;

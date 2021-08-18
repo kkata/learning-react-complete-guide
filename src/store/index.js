4fd04dcdb0ca40542eea3ca51556a30199f22d84
kkata <14849535+kkata@users.noreply.github.com>
2021-08-18 18:46:13 +0900

    243. Working with Multiple Slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,23 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = {
-  counter: 0,
+const initialCounterState = {
+  value: 0,
   showCounter: true,
 };
 
 const counterSlice = createSlice({
   name: "counter",
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       // reduxtoolkitがmutationしないようにしてくれるのでOK
-      state.counter++;
+      state.value++;
     },
     decrement(state) {
-      state.counter--;
+      state.value--;
     },
     increase(state, action) {
-      state.counter = state.counter + action.payload;
+      state.value = state.value + action.payload;
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
@@ -25,11 +25,30 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  // reducer: counterSlice.reducer, 1つのreducerの場合
   // reducer: { counter: counterSlice.reducer }, 複数のreducerがある場合
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 
 export default store;

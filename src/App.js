8a5d1668f3ce75abcbbe07bd1aff67e8734c5c77
kkata <14849535+kkata@users.noreply.github.com>
2021-08-20 20:42:13 +0900

    added starting files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-const App = () => {
-  return <div></div>;
-};
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+
+function App() {
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
+}
 
 export default App;
